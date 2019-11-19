a6414c4ca57bac2a05374be21c901036312a4aad
halon <halon.filip@gmail.com>
2019-11-19 22:53:19 +0100

    Added several functionalities to the ingredient search

--- vegan_spider_server/static/js/app.js
+++ vegan_spider_server/static/js/app.js
@@ -13,7 +13,42 @@ $(function() {
         )
     };
 
-    $('.js-example-basic-single').select2({
+    const $select2Ingredients = $('#select2-ingredients');
+
+    const addNewIngredientRow = function(id, name, photo) {
+        return $(`<ul>
+                    <input type="hidden" value="${id}">
+                    <li><img src="${photo}" alt="${name}"></li>
+                    <li>${name}</li>
+                    <li>
+                        <input name="quantity" type="number">
+                    </li>
+                    <li>
+                        <select name="unit">
+                            <option value="gram" selected>gram</option>
+                            <option value="kilogramow" selected>kilogramów</option>
+                            <option value="mililitrow">mililitrów</option>
+                            <option value="litrow">litrów</option>
+                            <option value="sztuk">sztuk</option>
+                        </select>
+                    </li>
+                    <li>
+                        <button class="ingredient delete">Usuń</button>
+                    </li>
+                </ul>`)
+    };
+
+    let displayedIngredients;
+
+    $('.ingredient.add').on("click", () => {
+        const choices = $select2Ingredients.select2('data');
+        console.log(choices);
+        $select2Ingredients.val(null).trigger('change');
+
+    });
+
+    $select2Ingredients.select2({
+        dropdownParent: $('#ingredient-search-form'),
         ajax: {
             delay: 250,
             url: 'http://127.0.0.1:8000/ingredients/',
